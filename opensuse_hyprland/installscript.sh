541848845682d4a9cda6063bf52e9f2f67ce6025
Creptite <64784798+Creptite@users.noreply.github.com>
2024-02-11 09:01:44 -0500

    Add files via upload

---
+++ opensuse_hyprland/installscript.sh
@@ -0,0 +1,3 @@
+sudo zypper -y install hyprland pipewire wireplumber xdg-desktop-portal-hyprland kitty polkit-kde-agent-6 qt6-wayland
+sudo zypper -y install waybar hyprpaper wofi hyprpicker clipman 
+
