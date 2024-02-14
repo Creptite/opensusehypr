b8c3cab6a20ec1e5873ffa0e8fd63130ee548444
Creptite <64784798+Creptite@users.noreply.github.com>
2024-02-13 19:44:15 -0500

    Delete opensuse_hyprland directory

--- opensuse_hyprland/installscript.sh
+++
@@ -1,3 +0,0 @@
-sudo zypper -y install hyprland pipewire wireplumber xdg-desktop-portal-hyprland kitty polkit-kde-agent-6 qt6-wayland
-sudo zypper -y install waybar hyprpaper wofi hyprpicker clipman 
-
